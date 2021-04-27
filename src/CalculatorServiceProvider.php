15ee539587d028aab074c4539c37f7c8d86a4c61
truongkin <truongkin189@gmail.com>
2021-04-27 14:48:56 +0700

    first

---
+++ src/CalculatorServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Truongkin\Calculator;
+
+use Illuminate\Support\ServiceProvider;
+
+class CalculatorServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        include __DIR__.'/route.php';
+    }
+}
